70d94a9f6472e2535b73f2e8ef3d21228028d9f4
Alex Turevski <me@turevski.com>
2017-08-13 17:49:44 +0300

    going forward

--- conf.js
+++ conf.js
@@ -1,4 +1,14 @@
+// conf.js
 exports.config = {
+  framework: 'jasmine',
   seleniumAddress: 'http://localhost:4445/wd/hub',
-  specs: ['spec.js']
-};
+  specs: ['spec.js'],
+  multiCapabilities: [
+    // {
+    //   browserName: 'firefox'
+    // },
+    {
+      browserName: 'chrome'
+    }
+  ]
+}
--- spec.js
+++ spec.js
@@ -1,38 +1,40 @@
-// describe('angularjs homepage todo list', function() {
-//   it('should add a todo', function() {
-//     browser.get('https://angularjs.org');
-
-//     element(by.model('todoList.todoText')).sendKeys('write first protractor test');
-//     element(by.css('[value="add"]')).click();
-
-//     var todoList = element.all(by.repeater('todo in todoList.todos'));
-//     expect(todoList.count()).toEqual(3);
-//     expect(todoList.get(2).getText()).toEqual('write first protractor test');
-
-//     // You wrote your first test, cross it off the list
-//     todoList.get(2).element(by.css('input')).click();
-//     var completedAmount = element.all(by.css('.done-true'));
-//     expect(completedAmount.count()).toEqual(2);
-//   });
-// });
-
+// spec.js
 describe('Protractor Demo App', function () {
-  it('should have a title', function () {
+  var firstNumber = element(by.model('first'));
+  var secondNumber = element(by.model('second'));
+  var goButton = element(by.id('gobutton'));
+  var latestResult = element(by.binding('latest'));
+  var history = element.all(by.repeater('result in memory'));
+
+  function add(a, b) {
+    firstNumber.sendKeys(a.toString());
+    secondNumber.sendKeys(b.toString());
+    goButton.click();
+  }
+
+  beforeEach(function () {
     browser.get('http://juliemr.github.io/protractor-demo/');
-
-    expect(browser.getTitle()).toEqual('Super Calculator');
   });
-});
 
-describe('Protractor Demo App', function () {
-  it('should add one and two', function () {
-    browser.get('http://juliemr.github.io/protractor-demo/');
-    element(by.model('first')).sendKeys(1);
-    element(by.model('second')).sendKeys(2);
+  it('should have a history', function () {
+    for(const t in Array.from(Array(5).keys())) {
+      add(t, parseInt(t)+1);
+    }
 
-    element(by.id('gobutton')).click();
+    expect(history.count()).toEqual(5);
 
-    expect(element(by.binding('latest')).getText()).
-      toEqual('5'); // This is wrong!
+    expect(history.last().getText()).toContain('0 + 1');
   });
+
+  it('should show an alert', () => {
+    browser.executeScript('alert("alert")');
+    var EC = protractor.ExpectedConditions;
+    // Waits for an alert pops up.
+    browser.wait(EC.alertIsPresent(), 5000);
+
+    var alertDialog = browser.switchTo().alert();
+
+    expect(alertDialog.getText()).toEqual("alert");
+  });
+
 });
