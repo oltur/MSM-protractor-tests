88a21632d8840eb55867c6afed9f7fdf40d3dbe1
Alex Turevski <olturua@gmail.com>
2017-08-19 22:22:02 +0300

    .

--- conf.js
+++ conf.js
@@ -1,12 +1,12 @@
 // conf.js
 exports.config = {
   framework: 'jasmine',
-  seleniumAddress: 'http://localhost:4445/wd/hub',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['spec.js'],
   multiCapabilities: [
-    {
-      browserName: 'firefox'
-    },
+    // {
+    //   browserName: 'firefox'
+    // },
     {
       browserName: 'chrome'
     }
--- spec.js
+++ spec.js
@@ -1,40 +1,93 @@
 // spec.js
-describe('Protractor Demo App', function () {
-  var firstNumber = element(by.model('first'));
-  var secondNumber = element(by.model('second'));
-  var goButton = element(by.id('gobutton'));
-  var latestResult = element(by.binding('latest'));
-  var history = element.all(by.repeater('result in memory'));
+describe('MSMS site tests', function () {
 
-  function add(a, b) {
-    firstNumber.sendKeys(a.toString());
-    secondNumber.sendKeys(b.toString());
-    goButton.click();
-  }
+  var t = 5000;
+  var d;
+
+  // var firstNumber = element(by.model('first'));
+  // var secondNumber = element(by.model('second'));
+  // var goButton = element(by.id('gobutton'));
+  // var latestResult = element(by.binding('latest'));
+  // var history = element.all(by.repeater('result in memory'));
+
+  // function add(a, b) {
+  //   firstNumber.sendKeys(a.toString());
+  //   secondNumber.sendKeys(b.toString());
+  //   goButton.click();
+  // }
+
+  //   browser.driver.wait(function() {
+  //     return browser.driver.findElement(by.id('id'))
+  //              .then(function(elem) {
+  //                elem.click();
+  //                return true;
+  //              });
+  //  }, timeout_interval);
 
   beforeEach(function () {
-    browser.get('http://juliemr.github.io/protractor-demo/');
+    d = browser.driver;
+    d.ignoreSynchronization = true;
+    d.get('https://www.mysupermarket.co.uk/');
   });
 
-  it('should have a history', function () {
-    for(const t in Array.from(Array(5).keys())) {
-      add(t, parseInt(t)+1);
-    }
 
-    expect(history.count()).toEqual(5);
+  // it('should have a title', () => {
 
-    expect(history.last().getText()).toContain('0 + 1');
+  //   expect(d.getTitle()).toContain('mySupermarket');
+  // });
+
+  it('should open a main page', () => {
+
+    d.findElement(By.className('StartShoppingBtn')).then(elem => {
+      elem.click();
+      console.log('0');
+      return true;
+    });
+
+    d.findElement(By.id('iframeForm')).then((elem) => {
+      d.switchTo().frame("iframeForm");
+      console.log('1');
+      return true;
+    });
+
+
+    d.wait(() => {
+      console.log('2b');
+      d.findElement(By.className('ui-dialog-titlebar-close')).then((elem) => {
+        console.log('2a');
+        elem.click();
+        d.switchTo().defaultContent();
+        console.log('2');
+        return true;
+      });
+    }, 60000);
+
+    d.findElement(By.id('ListTitle')).then((elem) => {
+      expect(elem.getText()).toEqual('my Top Offers');
+      return true;
+    });
+
   });
 
-  it('should show an alert', () => {
-    browser.executeScript('alert("alert")');
-    var EC = protractor.ExpectedConditions;
-    // Waits for an alert pops up.
-    browser.wait(EC.alertIsPresent(), 5000);
+  // it('should have a history', function () {
+  //   for(const t in Array.from(Array(5).keys())) {
+  //     add(t, parseInt(t)+1);
+  //   }
 
-    var alertDialog = browser.switchTo().alert();
+  //   expect(history.count()).toEqual(5);
 
-    expect(alertDialog.getText()).toEqual("alert");
-  });
+  //   expect(history.last().getText()).toContain('0 + 1');
+  // });
+
+  // it('should show an alert', () => {
+  //   browser.executeScript('alert("alert")');
+  //   var EC = protractor.ExpectedConditions;
+  //   // Waits for an alert pops up.
+  //   browser.driver.wait(EC.alertIsPresent(), 5000);
+
+  //   var alertDialog = browser.switchTo().alert();
+
+  //   expect(alertDialog.getText()).toEqual("alert");
+  // });
 
 });
