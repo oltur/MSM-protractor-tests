d8820e76d80920ad70cc129d1ca225df5cf892e4
Alex Turevski <me@turevski.com>
2017-08-22 10:41:48 +0300

    helpers!

--- tools/helpers.js
+++ tools/helpers.js
@@ -12,6 +12,14 @@ class Helpers {
         return this.findAndWaitForVisible(by).then(elem => elem.click());
     }
 
+    findAndSendKeys(by, keys) {
+        return this.findAndWaitForVisible(by).then(elem => elem.sendKeys(keys));
+    }
+
+    findAndExpectTextContain(by, textContain) {
+        return this.findAndWaitForVisible(by).then(elem => expect(elem.getText()).toContain(textContain));
+    }
+
     findAndWaitForVisible(by) {
         return this.d.findElement(by).then(elem => {
             return this.d.wait(until.elementIsVisible(elem)).then(() => Promise.resolve(elem));
@@ -19,60 +27,44 @@ class Helpers {
     }
 
     login() {
+        this.out.log('Finding iframeForm');
         var result =
             this.d.findElement(by.id('iframeForm'))
                 .then(elem => {
-                    this.out.log('Found iframeForm: ' + elem);
+                    this.out.log('Switching to iframeForm');
                     return this.d.switchTo().frame(elem)
                 })
                 .then(() => {
-                    this.out.log("Switching to iframeForm");
-                    return this.d.findElement(by.id('Email'))
+                    this.out.log("Filling Email");
+                    return this.findAndSendKeys(by.id('Email'), testData.accessData.userName)
                 })
                 .then((elem) => {
-                    this.out.log("Filling username");
-                    elem.sendKeys(testData.accessData.userName);
-                    return this.d.findElement(by.id('PasswordLogin'))
-                }).then((elem) => {
-                    this.out.log("Filling password");
-                    elem.sendKeys(testData.accessData.password);
-                    return this.d.findElement(by.id('SignInButton'))
+                    this.out.log("Filling PasswordLogin");
+                    return this.findAndSendKeys(by.id('PasswordLogin'), testData.accessData.password);
                 })
                 .then((elem) => {
                     this.out.log("Clicking SignInButton");
-                    return elem.click();
+                    return this.findAndClick(by.id('SignInButton'))
                 })
-                .then(() => {
-                    this.out.log("Finding ContinueButton");
-                    return this.d.findElement(by.id('ContinueButton'));
-                })
-                .then(elem => {
-                    this.out.log("Waiting for ContinueButton visibility");
-                    return this.d.wait(until.elementIsVisible(elem)).then(() => Promise.resolve(elem));
-                })
-                .then(elem => {
+                .then((elem) => {
                     this.out.log("Clicking ContinueButton");
-                    return elem.click();
+                    return this.findAndClick(by.id('ContinueButton'));
                 })
-                .then(() => {
+                .then((elem) => {
                     this.out.log("Switching to default");
                     return this.d.switchTo().defaultContent();
                 })
                 .then(() => {
                     this.d.sleep(5000);
-                    this.out.log("Finding and clicking StartShoppingBtn");
+                    this.out.log("Clicking StartShoppingBtn");
                     return this.findAndClick(by.className('StartShoppingBtn'));
                 })
                 .then(() => {
                     this.out.log("Finding ListTitle");
-                    return this.findAndWaitForVisible(by.id('ListTitle'))
-                })
-                .then(elem => {
-                    this.out.log("Checking text of Top Offers");
-                    return expect(elem.getText()).toContain(' Top Offers');
+                    return this.findAndExpectTextContain(by.id('ListTitle'), ' Top Offers')
                 });
         return result;
     }
 }
 
-exports.getInstance = (d) => new Helpers(d);
+exports.getInstance = (d, out) => new Helpers(d, out);
